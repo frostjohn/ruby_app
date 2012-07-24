271d7ac23ed5f6f551529a7acb27acd344bce7cb
John Frost <john@cisimple.com>
2012-07-24 00:34:00 -0700

    Finish user signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 end
+
+def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
-  def new
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      
+    else
+      render 'new'
+    end
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
 end
